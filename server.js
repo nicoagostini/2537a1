e1ea21d6ad293411dbb8a7779a7991ed99e5e525
nicoagostini <nic.voy@hotmail.com>
2025-04-28 23:13:59 -0700

    2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const session = require('express-session');
 
 dotenv.config();
 
@@ -10,6 +11,12 @@ const app = express();
 // app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: false
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
